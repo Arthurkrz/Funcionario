6b02f414013d3532463acd8088b58f524b5bf5f3
Arthur K <arthurk031@outlook.com>
2024-08-29 16:23:21 -0300

    Início

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao programa Calculador de Salario!\n\nInsira, linha por linha, o nome do funcionário, seu salário bruto e imposto deduzido (em porcentagem).");
+            string nome = Console.ReadLine();
+            double salarioBruto = double.Parse(Console.ReadLine());
+            int imposto = int.Parse(Console.ReadLine());
+            if (salarioBruto > 0 && imposto > 0)
+            {
+                Salario f = new Salario(nome, salarioBruto, imposto);
+                Console.Clear();
+                Console.WriteLine($"Funcionário {f.nome} apresenta salário líquido de R${f.CalcularSalario(f.salarioBruto, f.imposto)}.");
+            }
+            else
+            {
+                Console.WriteLine("Valores inválidos.");
+            }
+        }
+    }
+}
---
+++ Salario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Funcionario
+{
+    class Salario
+    {
+        public string nome;
+        public double salarioBruto;
+        public int imposto;
+        public double CalcularSalario(double salarioBruto, int imposto)
+        {
+            return salarioBruto - (salarioBruto * (imposto / 100.0));
+        }
+        public Salario(string nome, double salarioBruto, int imposto)
+        {
+            this.nome = nome;
+            this.salarioBruto = salarioBruto;
+            this.imposto = imposto;
+        }
+    }
+}
